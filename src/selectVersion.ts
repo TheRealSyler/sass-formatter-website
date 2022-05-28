b277b92a7bff4f4d2f3b84642353cc8262bf4c68
Leonard Grosoli <hileo77@gmail.com>
2022-05-28 14:57:28 +0200

    Only show available versions

--- src/selectVersion.ts
+++ src/selectVersion.ts
@@ -1,18 +1,16 @@
 import { setCurrentFormatter } from './index';
-
+import { formatters } from '../npm_packages/sass-formatter/superGlue'
 export async function InitFormatterVersionSelection() {
-  const registry = await import('../files/sass-formatter-registry.json');
 
   const versionSelector = document.getElementById('formatter-versions')! as HTMLSelectElement;
   versionSelector.disabled = false;
   versionSelector.innerHTML = '';
 
-  for (const version in registry.versions) {
+  for (const version in formatters) {
     const option = document.createElement('option');
     option.value = version;
     option.text = `sass-formatter@${version}`;
     option.selected = true;
-    option.className = 'TEST';
     setCurrentFormatter(version);
     versionSelector.prepend(option);
   }
